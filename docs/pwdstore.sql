9636c5e2a602f73f15ac3649a0c3aa61bc1310d7
mbleuel <mbleuel@bleuelmedia.com>
2012-12-28 01:11:29 +0100

    Update database dump.

--- docs/pwdstore.sql
+++ docs/pwdstore.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Dec 26, 2012 at 11:08 PM
+-- Generation Time: Dec 28, 2012 at 01:10 AM
 -- Server version: 5.5.25a
 -- PHP Version: 5.4.4
 
@@ -63,15 +63,24 @@ CREATE TABLE IF NOT EXISTS `ci_sessions` (
 
 CREATE TABLE IF NOT EXISTS `secrets` (
   `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `user_id` int(10) unsigned NOT NULL,
   `category` int(10) unsigned NOT NULL,
   `tags` tinytext,
-  `host` text,
+  `description` text,
   `secret` blob NOT NULL,
   `comment` text,
   `date` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
-  KEY `category` (`category`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  KEY `category` (`category`),
+  KEY `user_id` (`user_id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+
+--
+-- Dumping data for table `secrets`
+--
+
+INSERT INTO `secrets` (`id`, `user_id`, `category`, `tags`, `description`, `secret`, `comment`, `date`) VALUES
+(1, 1, 1, NULL, 'Windows XP Professional x64 EN', 0x6a586351644932656c70584a502f664852597248694c562b6a414b63394562596a3948614d4e4f792b626f75666933594448586661444868424f656751735056735a53366b4254496d68367867427a344e5a714d30513d3d, 'Kommentar', '2012-12-28 00:07:07');
 
 -- --------------------------------------------------------
 
@@ -88,7 +97,7 @@ CREATE TABLE IF NOT EXISTS `users` (
   `level` tinyint(4) NOT NULL DEFAULT '1',
   PRIMARY KEY (`id`),
   UNIQUE KEY `name` (`name`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Dumping data for table `users`
@@ -105,7 +114,8 @@ INSERT INTO `users` (`id`, `name`, `password`, `email`, `active`, `level`) VALUE
 -- Constraints for table `secrets`
 --
 ALTER TABLE `secrets`
-  ADD CONSTRAINT `secrets_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`id`);
+  ADD CONSTRAINT `secrets_ibfk_1` FOREIGN KEY (`category`) REFERENCES `categories` (`id`),
+  ADD CONSTRAINT `secrets_ibfk_2` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`);
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
